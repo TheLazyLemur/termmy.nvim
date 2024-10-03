8c8b8311836693bd89d0322a54f16cec4233ae23
danielrousseau <drousseau@luno.com>
2024-10-03 20:05:23 +0200

    Added config function for window config

--- lua/termmy/init.lua
+++ lua/termmy/init.lua
@@ -7,18 +7,9 @@ TERMMY.toggle = function(split)
     end
 
     local is_new = TERMMY.buf == nil or not vim.api.nvim_buf_is_valid(TERMMY.buf)
-    local cursor_pos = vim.fn.screenpos(0, vim.fn.line('.'), vim.fn.col('.'))
 
     if not split then
-        local win_config = {
-            relative = 'editor',
-            row = cursor_pos.row + 0,
-            col = cursor_pos.col - 1,
-            width = 60,
-            height = 10,
-            style = 'minimal',
-            border = 'single',
-        }
+        local win_config = TERMMY.config_func()
 
         if is_new then
             TERMMY.buf = vim.api.nvim_create_buf(false, true)
@@ -39,6 +30,26 @@ TERMMY.toggle = function(split)
     end
 
     TERMMY.buf = vim.api.nvim_get_current_buf()
+end
+
+TERMMY.setup = function(config_func)
+    if config_func ~= nil then
+        TERMMY.config_func = config_func
+    else
+        TERMMY.config_func = function()
+            local cursor_pos = vim.fn.screenpos(0, vim.fn.line('.'), vim.fn.col('.'))
+            local cfg = {
+                relative = 'editor',
+                row = cursor_pos.row + 0,
+                col = cursor_pos.col - 1,
+                width = 60,
+                height = 10,
+                style = 'minimal',
+                border = 'single',
+            }
+            return cfg
+        end
+    end
 end
 
 return TERMMY
